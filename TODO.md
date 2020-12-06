2a150aa49ba97d5e6c4cb9d713438d953152f11f
Daniel Lewis <github@mrdaniellewis.co.uk>
2020-12-06 12:37:49 +0000

    multiple page headers

--- TODO.md
+++ TODO.md
@@ -1,14 +1,12 @@
 # TODO
 
+- last page header and footer
 - update rules
   - breakBefore: auto, avoid, page - default auto
   - breakAfter: auto, avoid, page - default auto
   - breakInside: auto, avoid - default auto
   - orphans (inherited) - default 2
   - widows (inherited) - default 2
-- Multiple headers
-- Multiple footers
-- Update placeholders
 
 - Tests for different rules
 - cache rootRect
--- src/create_pages.js
+++ src/create_pages.js
@@ -1,6 +1,11 @@
 import { extractOverflow } from './extract_overflow';
+import { parseAnPlusB } from './parse_an_plus_b';
 
-function newPage({ footer, header, pageNumber }) {
+function findForPage(nodes, pageNumber) {
+  return nodes.find(({ match }) => match(pageNumber))?.fragment;
+}
+
+function newPage({ footers, headers, pageNumber }) {
   const page = document.createElement('div');
   page.classList.add('page');
   page.setAttribute('role', 'region');
@@ -9,6 +14,7 @@ function newPage({ footer, header, pageNumber }) {
   page.style.setProperty('--paged-page-number', pageNumber);
   document.body.appendChild(page);
 
+  const header = findForPage(headers, pageNumber);
   if (header) {
     const pageHeader = document.createElement('div');
     pageHeader.classList.add('page-header');
@@ -20,6 +26,7 @@ function newPage({ footer, header, pageNumber }) {
   pageContent.classList.add('page-content');
   page.appendChild(pageContent);
 
+  const footer = findForPage(footers, pageNumber);
   if (footer) {
     const pageFooter = document.createElement('div');
     pageFooter.classList.add('page-footer');
@@ -41,30 +48,17 @@ function getStartingContent() {
   return content;
 }
 
-function getHeader() {
-  const header = document.querySelector('body > header');
-  if (!header) {
-    return null;
-  }
-  const content = document.createDocumentFragment();
-  Array.from(header.childNodes).forEach((child) => {
-    content.appendChild(child);
-  });
-  header.remove();
-  return content;
-}
-
-function getFooter() {
-  const footer = document.querySelector('body > footer');
-  if (!footer) {
-    return null;
-  }
-  const content = document.createDocumentFragment();
-  Array.from(footer.childNodes).forEach((child) => {
-    content.appendChild(child);
-  });
-  footer.remove();
-  return content;
+function extractSelector(selector) {
+  return Array
+    .from(document.querySelectorAll(selector))
+    .map((node) => {
+      const fragment = document.createDocumentFragment();
+      Array.from(node.childNodes).forEach((child) => {
+        fragment.appendChild(child);
+      });
+      node.remove();
+      return { fragment, match: parseAnPlusB(node.dataset.pagerPageSelector) };
+    });
 }
 
 function empty(node) {
@@ -79,14 +73,14 @@ function empty(node) {
  * Move the content into pages
  */
 export function createPages({ rules = [] } = {}) {
-  const header = getHeader();
-  const footer = getFooter();
+  const headers = extractSelector('body > header');
+  const footers = extractSelector('body > footer');
   let content = getStartingContent();
   let pageCount = 0;
 
   while (content && !empty(content)) {
     pageCount += 1;
-    const page = newPage({ footer, header, pageNumber: pageCount });
+    const page = newPage({ footers, headers, pageNumber: pageCount });
     page.appendChild(content);
     content = extractOverflow(page, { rules });
   }
