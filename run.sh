525a954ee2c0028a7ed69b0351fe0ac8f39260d1
Pratik Sharma <pratiksharma@microsoft.com>
2021-02-16 20:56:58 +0530

    maxWorkers=1

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 echo "Running baseline benchmarking tests..."
 git config --global user.email "pratiksharma@microsoft.com"
 git config --global user.name "Pratik Sharma"
+git config --global github.user "pratiksharma23"
+git config --global github.token "248a22e97d0847eca79f825c7aef93952694019d"
 for i in {1..25..3}
 	  do 
 		  sed -i -e "s/jest/jest --maxWorkers=$i/g" package.json
